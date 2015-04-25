47ec6193e607d1d1a7d9f86b8b95069fb9458e33
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-04-25 00:37:40 -0500

    finished read_pgm()

--- pgmio.py
+++ pgmio.py
@@ -1,11 +1,12 @@
 import array
+import numpy as np
 
 class FileIOException(Exception):
     pass
 
 
 def write_pgm(numpyarray, file_directory=None):
-    if file_directory == None:
+    if file_directory is None:
         FileIOException('You must specify a file directory!')
 
     f = open(file_directory, "wb")
@@ -18,7 +19,25 @@ def write_pgm(numpyarray, file_directory=None):
     _buffer.tofile(f)
     f.close()
 
+
 def read_pgm(file_directory=None):
+    if file_directory is None:
+        FileIOException('You must specify a file directory!')
+
     f = open(file_directory)
-    pgm
+    # need to trash the first couple of lines
+    f.readline()
+    f.readline()
+    f.readline()
+    f.readline()
+    # Next f.readline() will the numbers
+    a = "[" + f.readline()
+    a = a.replace("\xff", "255,")
+    a = a.replace("\x00", "0,")
+    a = a.rstrip(",")
+    a += "]"
     
+    f.close()
+    return np.array(eval(a))
+
+
